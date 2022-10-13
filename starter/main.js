ddc49b97b48ef8b3d1476ad1a93609bfe2888d46
stephen <stephenadewumi078@.com>
2022-10-13 05:55:42 -0700

    commit message

---
+++ starter/main.js
@@ -0,0 +1,15 @@
+
+//ESS
+
+// console.log('send me your account number')
+// alert(writeup)
+// var writeup ='I am hungry'
+
+// console.log(harmburgerdiv)
+// console.log(document)
+
+var harmburgerdiv = document.querySelector('.harmburger')
+
+function dosomething(){
+    harmburgerdiv.classList.toggle('showburger')
+}
