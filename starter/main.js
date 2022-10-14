0eea23b4610de28758e932207772c10d1013c581
stephen <stephenadewumi078@.com>
2022-10-14 04:50:41 -0700

    completed first section for small and large devices

--- starter/main.js
+++ starter/main.js
@@ -9,7 +9,15 @@
 // console.log(document)
 
 var harmburgerdiv = document.querySelector('.harmburger')
+var mobilelinks = document.querySelector('.mobile-links-holder')
+var mybackdrop = document.querySelector('.backdrop')
+
+
 
 function dosomething(){
     harmburgerdiv.classList.toggle('showburger')
+    
+    mobilelinks.classList.toggle('show-mobile-links-holder')
+  mybackdrop.classList.toggle('show-backdrop')
+
 }
